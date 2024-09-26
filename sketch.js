5f0107f894465ddc70fcdc9d9a8ee0e9395dcb88
joaogabreilsoares <162469556+joaogabreilsoares@users.noreply.github.com>
2024-09-26 12:07:37 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+// ação, comédia, ficção científica, terror, romance, guerra
+
+// gente grande, 12, comédia 
+// guardiões da galáxia, 12, ação 
+// homem aranha, LIVRE, ação, ficção científica
+// planeta dos macacos, 12, ação 
+// godzilla vs kong, 12, ação
+// velozes e furiosos 2, 14, ação 
+// Deadpool e Wolverine, 18, comédia, ficção científica
+// o telefone preto, 16, terror 
+// lobo de wall street, 18, comédia
+// titanic, 12, romance 
+// o menino do pijama listrado, 12, guerra
+// a freira, 14, terror
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+ background(220);
+ let recomendacao = "lobo de wall street";
+  text(recomenacao, width / 2, height / 2);
+}
+
+
+
+
+
+
+
+
+
